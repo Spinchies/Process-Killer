45efd687720ab55d2efc5160b4b8cca825f87f99
kayla <111674229+Spinchies@users.noreply.github.com>
2023-04-19 21:29:16 -0500

    oki

--- assist/window.Designer.cs
+++ assist/window.Designer.cs
@@ -29,93 +29,142 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(window));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            listBox1 = new ListBox();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.LightGray;
-            this.button1.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(84, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(188, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "GTA 5";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.BackColor = Color.LightGray;
+            button1.Font = new Font("Ebrima", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(341, 389);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 43);
+            button1.TabIndex = 0;
+            button1.Text = "GTA 5";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.LightGray;
-            this.button2.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(84, 159);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(188, 41);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cheat Engine";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            button2.BackColor = Color.LightGray;
+            button2.Font = new Font("Ebrima", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(12, 389);
+            button2.Name = "button2";
+            button2.Size = new Size(188, 43);
+            button2.TabIndex = 1;
+            button2.Text = "Cheat Engine";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.LightGray;
-            this.button3.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(84, 113);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(188, 40);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Custom";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            button3.BackColor = Color.LightGray;
+            button3.Font = new Font("Ebrima", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(178, 320);
+            button3.Name = "button3";
+            button3.Size = new Size(122, 40);
+            button3.TabIndex = 2;
+            button3.Text = "Scan";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.textBox1.Location = new System.Drawing.Point(84, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "Custom App Name";
-            this.textBox1.Size = new System.Drawing.Size(188, 24);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            textBox1.Font = new Font("Ebrima", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.ForeColor = SystemColors.WindowFrame;
+            textBox1.Location = new Point(178, 129);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Custom App Name";
+            textBox1.Size = new Size(188, 24);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(310, 259);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(57, 70);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(247, 380);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(64, 64);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(178, 159);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(188, 109);
+            listBox1.TabIndex = 5;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(178, 274);
+            button4.Name = "button4";
+            button4.Size = new Size(122, 40);
+            button4.TabIndex = 6;
+            button4.Text = "Kill";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(309, 274);
+            button5.Name = "button5";
+            button5.Size = new Size(57, 86);
+            button5.TabIndex = 7;
+            button5.Text = "Clear";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(491, 12);
+            button6.Name = "button6";
+            button6.Size = new Size(38, 27);
+            button6.TabIndex = 8;
+            button6.Text = "help";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // window
             // 
-            this.AcceptButton = this.button3;
-            this.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(360, 321);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.Name = "window";
-            this.Text = "Process Killer";
-            this.Load += new System.EventHandler(this.window_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AcceptButton = button4;
+            BackColor = Color.CornflowerBlue;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(541, 444);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(listBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = SystemColors.Desktop;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "window";
+            Text = "Process Killer";
+            Load += window_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         private void window_Load(object sender, EventArgs e)
@@ -130,5 +179,9 @@
         private Button button3;
         private TextBox textBox1;
         private PictureBox pictureBox1;
+        public ListBox listBox1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- assist/window.cs
+++ assist/window.cs
@@ -1,16 +1,21 @@
 using System.Diagnostics;
-
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace assist
 {
+
+
+
     public partial class window : Form
     {
         public window()
         {
-
             InitializeComponent();
-            
-            
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -22,7 +27,7 @@ namespace assist
                 foreach (var process in Process.GetProcessesByName("GTA5"))
                 {
                     process.Kill();
-                    MessageBox.Show(":3");
+                    MessageBox.Show("Application Successfully Killed <3");
                 }
 
         }
@@ -36,28 +41,98 @@ namespace assist
                 foreach (var process in Process.GetProcessesByName("cheatengine-x86_64-SSE4-AVX2"))
                 {
                     process.Kill();
-                    MessageBox.Show(":3");
+                    MessageBox.Show("Application Successfully Killed <3");
+                }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+
+                var RAM = Process.GetProcesses();
+                var procs = RAM.Where(x => x.SessionId == Process.GetCurrentProcess().SessionId)
+                               .OrderByDescending(x => x.PrivateMemorySize64)
+                               .ToList();
+
+                foreach (var p in procs)
+                {
+                    listBox1.Items.Add(p.ProcessName);
+                }
+            }
+        }
+            
+        class ProcessWrapper
+        {
+            private Process _process;
+
+            public ProcessWrapper(Process process)
+            {
+                _process = process;
+            }
+
+            public override string ToString()
+            {
+                return _process.ToString() + " (" + _process.PrivateMemorySize64 / 1000000L + "M)";
+            }
+
+        }
+
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = listBox1.GetItemText(listBox1.SelectedItem);
+
+            Process[] pname = Process.GetProcessesByName(text);
+            if (pname.Length != 0)
+                foreach (var process in Process.GetProcessesByName(text))
+                {
+                    process.Kill();
+                    MessageBox.Show("Application Successfully Killed <3");
+                }
+
+            string pgm = textBox1.Text;
+            Process[] pname1 = Process.GetProcessesByName(pgm);
+            if (pname1.Length != 0)
+                foreach (var process in Process.GetProcessesByName(pgm))
+                {
+                    process.Kill();
+                    MessageBox.Show("Application Successfully Killed <3");
                 }
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
             string program = textBox1.Text;
 
-            Process[] pname = Process.GetProcessesByName(program);
-            if (pname.Length == 0)
-                MessageBox.Show("Application not running :(");
-            else
-                foreach (var process in Process.GetProcessesByName(program))
-                {
-                    process.Kill();
-                    MessageBox.Show(":3");
-                }
+            var RAM = Process.GetProcesses();
+            var procs = RAM.Where(x => x.SessionId == Process.GetCurrentProcess().SessionId)
+                           .OrderByDescending(x => x.PrivateMemorySize64)
+                           .ToList();
+
+            foreach (var p in procs)
+            {
+                listBox1.Items.Clear();
+            }
 
         }
+        
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("due to my own inability to code, you have to clear the listbox before rescanning :(");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
